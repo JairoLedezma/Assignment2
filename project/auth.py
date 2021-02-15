db95eec72baa8818b59bbd856f919c84aef1ec5e
Jairo Ledezma <ledezmajairo20@gmail.com>
2021-02-14 23:40:41 -0600

    Created login, register, log out

--- project/auth.py
+++ project/auth.py
@@ -7,6 +7,8 @@ from .models import User
 from . import db
 
 auth = Blueprint('auth', __name__)
+#when creating a new page you have to render it first
+#then do the things after
 
 @auth.route('/login')
 def login():
@@ -14,11 +16,11 @@ def login():
 
 @auth.route('/login', methods=['POST'])
 def login_post():
-    email = request.form.get('email')
+    name = request.form.get('name')
     password = request.form.get('password')
     remember = True if request.form.get('remember') else False
 
-    user = User.query.filter_by(email=email).first()
+    user = User.query.filter_by(name=name).first()
 
     # check if user actually exists
     # take the user supplied password, hash it, and compare it to the hashed password in database
@@ -37,18 +39,19 @@ def signup():
 @auth.route('/signup', methods=['POST'])
 def signup_post():
 
-    email = request.form.get('email')
+    #email = request.form.get('email')
     name = request.form.get('name')
     password = request.form.get('password')
 
-    user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
+    #if decide to implement email just change email with a different var
+    user = User.query.filter_by(name=name).first() # if this returns a user, then the email already exists in database
 
     if user: # if a user is found, we want to redirect back to signup page so user can try again  
-        flash('Email address already exists')
+        flash('Name already exists')
         return redirect(url_for('auth.signup'))
 
     # create new user with the form data. Hash the password so plaintext version isn't saved.
-    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+    new_user = User( name=name, password=generate_password_hash(password, method='sha256'))
 
     # add the new user to the database
     db.session.add(new_user)
--- project/models.py
+++ project/models.py
@@ -3,8 +3,16 @@
 from flask_login import UserMixin
 from . import db
 
+#this is the information that will be placed into the sqlAlchemy
+#turns the class into a column in a data set
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
-    email = db.Column(db.String(100), unique=True)
+    #email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+    Address_1=db.Column(db.String(1000))
+    Address_2 = db.Column(db.String(1000))
+    City= db.Column(db.String(1000))
+    State=db.Column(db.String(2))
+    City = db.Column(db.String(9))
